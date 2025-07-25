3f971b651b5cc5d63c7a730b2fbab350c33fe64b
Jomar Milan <jomarm@jomarm.com>
2025-07-24 19:17:36 -0700

    Pick up work

---
+++ MonocleDevTool/Program.cs
@@ -0,0 +1,57 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using CodeChicken.DiffPatch;
+using ICSharpCode.Decompiler.CSharp.ProjectDecompiler;
+using ICSharpCode.Decompiler.Metadata;
+
+const string assemblyName = "Find_You, Version=1.0.4.0, Culture=neutral, PublicKeyToken=null";
+
+void RecursiveCopyDirectory(DirectoryInfo source, DirectoryInfo target)
+{
+    foreach (FileInfo file in source.GetFiles())
+    {
+        file.CopyTo(Path.Combine(target.FullName, file.Name), overwrite: true);
+    }
+
+    foreach (DirectoryInfo directory in source.GetDirectories())
+    {
+        RecursiveCopyDirectory(directory, target.CreateSubdirectory(directory.Name));
+    }
+}
+
+if (args.Length < 1)
+{
+    Console.WriteLine("Provide the Find_You assembly as an argument");
+    return 1;
+}
+
+// see TargetFramework assembly attribute
+UniversalAssemblyResolver resolver = new UniversalAssemblyResolver(args[0], true, ".NETCoreApp,Version=v6.0");
+// These are where libraries are located in the game installation
+resolver.AddSearchDirectory(Path.Combine(Path.GetDirectoryName(args[0]), "libraries"));
+WholeProjectDecompiler decompiler = new WholeProjectDecompiler(resolver);
+
+DirectoryInfo destDir = new DirectoryInfo("decompiled");
+
+if (!destDir.Exists)
+    destDir.Create();
+
+decompiler.DecompileProject(resolver.Resolve(AssemblyNameReference.Parse(assemblyName)), destDir.FullName);
+
+DirectoryInfo srcDir = new DirectoryInfo("src");
+
+RecursiveCopyDirectory(destDir, srcDir);
+
+DirectoryInfo patchDir = new DirectoryInfo("patches");
+if (patchDir.Exists)
+{
+    IEnumerable<FileInfo> files = patchDir.EnumerateFiles("*.patch", SearchOption.AllDirectories);
+    foreach (FileInfo file in files)
+    {
+        FilePatcher patcher = FilePatcher.FromPatchFile(file.FullName);
+        patcher.Patch(Patcher.Mode.FUZZY);
+        patcher.Save();
+    }
+}
+
+return 0;
--- README.md
+++ README.md
@@ -1,3 +1,21 @@
 # Monocle
 
-Mod loader for Find You
+Mod loader for Find You, a game by Byting Games. Monocle is licensed under the MIT license. Find You is licensed under
+[CC BY-NC-ND 4.0](https://creativecommons.org/licenses/by-nc-nd/4.0/deed.en).
+
+## Development
+
+Run MonocleDevTool with the Find You installation directory as the argument. In the current directory, the game assembly
+will be decompiled to `decompiled/` and `src/`. Make changes in `src/`, and generate patches using diffutils like this:
+
+```sh
+diff -u decompiled/Find_You/Game1.cs src/Find_You/Game1.cs -L decompiled/Find_You/Game1.cs -L src/Find_You/Game1.cs > patches/Find_You/Game1.cs.patch
+```
+
+where the modified file is `src/Find_You/Game1.cs`.
+
+### Why??
+
+For fun. I thought it would be interesting to try developing something like tModLoader where the game is decompiled and
+then modified instead of using plugin frameworks like BepInEx. Being able to directly edit a decompiled version also
+makes writing code that integrates with the game way easier, like being able to add to the enum `Find_You.Menu.Tab`.
